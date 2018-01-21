94e63c09bf455f0ded65641f5b0603e8555834a6
Anunai Ishan <ishananunai@gmail.com>
2018-01-20 19:47:37 -0500

    Update content for Spring 2018 semester

--- js/modifyWebsiteContent.js
+++ js/modifyWebsiteContent.js
@@ -2,24 +2,21 @@ $(document).ready(function() {
 	
 	// append education details
 	$('#education').append('Sophomore with major in <em>Computer Science</em> and minor in <em>Management</em><br>');
-	$('#education').append('Interested in <em>Machine Learning</em>, <em>Security</em> and <em>Database & Information Systems</em>');
+	$('#education').append('Interested in <em>Machine Learning</em> and <em>Software Engineering</em>');
 	
 	// append classes taking
-	$('#coursework').append('<b>Coursework - Fall 2017</b><br>');
-	$('#coursework').append('<b>CS 251</b> Data Structures and Algorithms<br>');
-	$('#coursework').append('<b>CS 250</b> Computer Architecture<br>');
-	$('#coursework').append('<b>MGMT 200</b> Introductory Accounting<br>');
+	$('#coursework').append('<b>Coursework - Spring 2018</b><br>');
+	$('#coursework').append('<b>CS 252</b> Systems Programming<br>');
+	$('#coursework').append('<b>CS 307</b> Software Engineering I<br>');
+	$('#coursework').append('<b>CS 390-CP1</b> Competitive Programming<br>');
+	$('#coursework').append('<b>MGMT 201</b> Management Accounting<br>');
+	$('#coursework').append('<b>ECON 251</b> Microeconomics<br>');
 
 	// append books
 	$('#books').append('<b>Books</b><br>');
 	$('#books').append('<em>How to Fly a Horse</em> by Kevin Ashton<br>');
 	$('#books').append('<em>The American Spirit</em> by David McCullough<br>');
 	$('#books').append('<em>Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future</em> by Ashlee Vance<br>');
+	$('#books').append('<em>The Professional: Defining the New Standard of Excellence at Work</em> by Subroto Bagchi<br>');
 	$('#books').append('<em>The Soul of Rumi</em> by Coleman Barks<br>');
-
-	// append hackathons
-	$('#hackathons').append('<b>Hackathons</b><br>');
-	$('#hackathons').append('<b>BigRed//Hacks 2016</b> - Cornell University<br>');
-	$('#hackathons').append('<b>HackGT 2016</b> - Georgia Institute of Technology<br>');
-	$('#hackathons').append('<b>HackIllinois 2017</b> - University of Illinois at Urbana–Champaign<br>');
 });
